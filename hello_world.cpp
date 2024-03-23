8e7f744887aca46ffd3e4ad5fb65980d9ba23d0c
AndrewB203 <bolshakov17419@gmail.com>
2024-03-23 20:52:34 +0300

    CHECK ERROR

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 	std::string name; // Create variable name
-	std::cout <<"Your name" << std::endl; //Outout messange about your name 
+	std::cout <<"Your name" << std::endl; // Outut messange about your name 
 	std::cin>> name; // input your name 
 	std::cout<<"Hello world from "<< name << std::endl; //Output messange
 	return 0;
